1f667380616fbcb50c4f85e04ecb8b7416ad0b53
JPMends <139639644+jpmendes2000@users.noreply.github.com>
2025-03-12 16:39:44 -0300

    Update App.java

--- Aplication/src/App.java
+++ Aplication/src/App.java
@@ -2,14 +2,32 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        /*
+         * Dev: JPMendes
+         * Ação: ligar páginas "DatabaseConnection" e "UserDAO"
+         */
         UserDAO userDAO = new UserDAO();
         userDAO.listUsers();
 
+        /*
+         * Dev: JPMendes
+         * Ação: ativar e crair variavel para o Scanner, importante para ler as informações 
+         */
         Scanner myObj = new Scanner(System.in);
         Scanner myInt = new Scanner(System.in);
 
+        /*
+         * Dev: JPMendes
+         * Ação: permite ver o código de barras do produto para cconferir se existe ou não
+         * Colocar: transformar em uma matriz e passar os valores do banco de dados para cá
+         */
         String cod_ja_registrado = "395023423";
 
+        /*
+         * Dev: JPMendes
+         * Ação: início da parte visivel do código
+         * Colocar: colocar um Switch que funcione direito
+         */  
         System.out.println("informe o código de barras");
         String cod_barras  = myObj.nextLine();
         System.out.println("confirme seu código: " + cod_barras);
@@ -64,3 +82,4 @@ public class App {
         myObj.close();
     }
 }
+
